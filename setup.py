9db4ac318369209ac23aa017cac71b70e4d71d4f
Bianca Rhodes <brhodes@nvidia.com>
2019-06-27 17:14:15 -0400

    Adding docker and python build file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+install_requires = [
+    'confluent_kafka',
+]
+
+setup(name='rapidscyber',
+      version='0.1.0',
+      description='RAPIDS Cyber',
+      author='NVIDIA Corporation',
+      packages=find_packages(include=['rapidscyber', 'rapidscyber.*']),
+      install_requires=install_requires
+      )
