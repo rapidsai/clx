95841e5458565e1d1206a9e181cbf4fb14d1bbcb
Bianca Rhodes <brhodes@nvidia.com>
2019-08-05 13:55:42 +0000

    Updated kafka reader for configurable time window

--- rapidscyber/io/reader/kafka_reader.py
+++ rapidscyber/io/reader/kafka_reader.py
@@ -5,10 +5,11 @@ from confluent_kafka import KafkaError
 
 # KafkaReader class
 class KafkaReader:
-    def __init__(self, batch_size, consumer):
+    def __init__(self, batch_size, consumer, time_window=30):
         self._batch_size = batch_size
         self._consumer = consumer
         self._has_data = True
+        self._time_window = time_window
 
     @property
     def consumer(self):
@@ -23,7 +24,6 @@ class KafkaReader:
         rec_cnt = 0
         running = True
         current_time = time.time()
-        time_window = 30
         try:
             while running:
                 msg = self.consumer.poll(timeout=1.0)
@@ -33,7 +33,7 @@ class KafkaReader:
                     data = msg.value().decode("utf-8")
                     if (
                         rec_cnt < self._batch_size
-                        and (time.time() - current_time) < time_window
+                        and (time.time() - current_time) < self.time_window
                     ):
                         events.append(data)
                         rec_cnt += 1
@@ -52,8 +52,6 @@ class KafkaReader:
             logging.error("closing kafka consumer...")
             self.close_consumer()
             logging.error("closed kafka consumer...")
-            sys.stderr.write("%% Aborted by user\n")
-            logging.error("%% Aborted by user\n")
 
     def close_consumer(self):
         if self.consumer is not None:
