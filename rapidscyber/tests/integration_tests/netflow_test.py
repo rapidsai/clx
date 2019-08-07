357639d9b02891e9bdc491e0231c2a1101458138
Bianca Rhodes <brhodes@nvidia.com>
2019-08-07 15:40:52 -0400

    Adding integration test

--- rapidscyber/tests/integration_tests/netflow_test.py
+++ rapidscyber/tests/integration_tests/netflow_test.py
@@ -1,7 +1,7 @@
 import logging
 import time
-#import multiprocessing
-logging.basicConfig(filename="run_it.out", level=logging.DEBUG)
+import multiprocessing
+logging.basicConfig(filename="run_integration_test.out", level=logging.DEBUG)
 
 from confluent_kafka.admin import AdminClient, NewTopic, NewPartitions, ConfigResource, ConfigSource
 from confluent_kafka import Producer, Consumer
@@ -10,24 +10,33 @@ from rapidscyber.workflow import netflow_workflow
 bootstrap_server = "kafka:29092"
 topics = ["input", "cyber-enriched-data"]
 msgs = ["cyber test 1", "cyber test 2"]
+producer = None
 consumer = None
-wf = None
-produce = None
 
 def setup():
     print("Begin test setup...")
     create_kafka_topics()
-    global wf
+    create_producer(bootstrap_server)
     wf = create_workflow([topics[0]], topics[1], bootstrap_server)
     print("Test setup complete.")
 
-def create_kafka_topics():
+def create_kafka_topics(topics):
     # Create Kafka topics
-    kafka_admin = AdminClient({'bootstrap.servers': bootstrap_server})
+    kafka_admin = AdminClient({'bootstrap.servers': 'kafka:29092'})
     kafka_topics = [NewTopic(topic, num_partitions=1, replication_factor=1) for topic in topics]
     fs = kafka_admin.create_topics(kafka_topics)
     print("Kafka topics created.")
     return fs
+
+def create_producer(bootstrap_server):
+    # Send cyber messages to input kafka topic
+    producer_conf = {
+        "bootstrap.servers": bootstrap_server,
+        "session.timeout.ms": 10000,
+    }
+    producer = Producer(producer_conf)
+    print("Kafka producer created.")
+    return producer
 
 def create_workflow(input_topics, output_topic, boostrap_server):
     # Create workflow
@@ -56,51 +65,35 @@ def run_workflow():
     wf.run_workflow()
 
 def send_data():
-    # Send cyber messages to input kafka topic
-    producer_conf = {
-        "bootstrap.servers": bootstrap_server,
-        "session.timeout.ms": 10000,
-    }
-    producer = Producer(producer_conf)
-    print("Kafka producer created.")
     for msg in msgs:
         print("Sending msg to workflow: " + msg)
-        producer.produce(topics[0], msg)
-
-def teardown():
-    print("teardown")
+        producer.produce(topic_names[0], msg)
 
 def verify():
+    print("teardown")
+
+def teardown():
     print("Verifying messages were processed by workflow...")
     consumer_conf = {
         "bootstrap.servers": "kafka:29092",
         "group.id": "int-test",
         "session.timeout.ms": 10000,
-        "default.topic.config": {"auto.offset.reset": "smallest"}
+        "default.topic.config": {"auto.offset.reset": "largest"}
     }
     consumer = Consumer(consumer_conf)
-    consumer.subscribe([topics[1]])
-    for i in range(1,10):
-        time.sleep(1)
+    consumer.subscribe(topic_names[1])
+    for msg in msgs:
         enriched_msg = consumer.poll(timeout=1.0)
         print("Enriched msg processed... " + enriched_msg)
 
 def main():
     setup()
-    send_data()
-    time.sleep(15)
-    run_workflow()
-    #time.sleep(15)
-    #process_run_workflow = multiprocessing.Process(target=run_workflow)
-    #process_send_data = multiprocessing.Process(target=send_data)
-    #process_run_workflow.start()
-    #time.sleep(10)
-    #process_send_data.start()
-    #time.sleep(10)
-    #process_send_data.terminate()
-    #process_run_workflow.terminate()
+    process_run_workflow = multiprocessing.Process(target=run_workflow)
+    process_send_data = multiprocessing.Process(target=send_data)
+    process_run_workflow.start()
+    process_send_data.start()
+    time.sleep(10)
+    process_send_data.terminate()
+    process_run_workflow.terminate()
     verify()
     teardown()
-
-if __name__ == "__main__":
-    main()
