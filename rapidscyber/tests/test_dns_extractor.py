f2e23e37b9f9b2ede9a37eea035b1a7255ff86db
bsuryadevara <bhargavsuryadevara@gmail.com>
2019-09-06 10:17:40 -0500

    Reorganized code structure.

--- rapidscyber/tests/test_dns_extractor.py
+++ rapidscyber/tests/test_dns_extractor.py
@@ -1,19 +1,8 @@
-import os
-import cudf
 import pytest
-from rapidscyber.dns.dns_extractor import DNSExtractor
+from cudf import DataFrame
+from rapidscyber.dns import dns_extractor as dns
 
-test_suffix_list_path = "%s/input/suffix_list.txt" % os.path.dirname(
-    os.path.realpath(__file__)
-)
-# Read suffix list csv file
-suffix_df = cudf.io.csv.read_csv(
-    test_suffix_list_path, names=["suffix"], header=None, dtype=["str"]
-)
-# Filter commented lines from suffix.
-suffix_df = suffix_df[suffix_df["suffix"].str.contains("^[^//]+$")]
-
-input_df = cudf.DataFrame(
+input_df = DataFrame(
     [
         (
             "url",
@@ -36,10 +25,15 @@ input_df = cudf.DataFrame(
 )
 
 
-@pytest.mark.parametrize("suffix_df", [suffix_df])
+def test_dns_vars_Provider():
+    sv = dns.DnsVarsProvider()
+    sv2 = dns.DnsVarsProvider()
+    assert sv is sv2
+
+
 @pytest.mark.parametrize("input_df", [input_df])
-def test_parse_url(suffix_df, input_df):
-    expected_output_df = cudf.DataFrame(
+def test_parse_url(input_df):
+    expected_output_df = DataFrame(
         [
             (
                 "domain",
@@ -77,15 +71,13 @@ def test_parse_url(suffix_df, input_df):
             ),
         ]
     )
-    dns_extractor = DNSExtractor(suffix_df)
-    output_df = dns_extractor.parse_url(input_df["url"], req_cols=["domain", "suffix"])
+    output_df = dns.parse_url(input_df["url"], req_cols=["domain", "suffix"])
     assert output_df.equals(expected_output_df)
 
 
-@pytest.mark.parametrize("suffix_df", [suffix_df])
 @pytest.mark.parametrize("input_df", [input_df])
-def test2_parse_url(suffix_df, input_df):
-    expected_output_df = cudf.DataFrame(
+def test2_parse_url(input_df):
+    expected_output_df = DataFrame(
         [
             (
                 "hostname",
@@ -157,14 +149,13 @@ def test2_parse_url(suffix_df, input_df):
             ),
         ]
     )
-    dns_extractor = DNSExtractor(suffix_df)
-    output_df = dns_extractor.parse_url(input_df["url"])
+    output_df = dns.parse_url(input_df["url"])
     assert output_df.equals(expected_output_df)
 
 
 @pytest.mark.parametrize("input_df", [input_df])
 def test_extract_hostname(input_df):
-    expected_output_df = cudf.DataFrame(
+    expected_output_df = DataFrame(
         [
             (
                 "hostname",
@@ -185,5 +176,68 @@ def test_extract_hostname(input_df):
             )
         ]
     )
-    output = DNSExtractor.extract_hostnames(input_df["url"])
+    output = dns.extract_hostnames(input_df["url"])
     assert output.equals(expected_output_df["hostname"])
+
+
+def test_get_hostname_split_df():
+    input_df = DataFrame(
+        [("hostname", ["forums.news.cnn.com.ac", "forums.news.cnn.ac", "b.cnn.com"])]
+    )
+
+    expected_output_df = DataFrame(
+        [
+            (4, ["ac", "", ""]),
+            (3, ["com", "ac", ""]),
+            (2, ["cnn", "cnn", "com"]),
+            (1, ["news", "news", "cnn"]),
+            (0, ["forums", "forums", "b"]),
+        ]
+    )
+    actual_output_df = dns.get_hostname_split_df(input_df["hostname"])
+    assert actual_output_df.equals(expected_output_df)
+
+
+def test_generate_tld_cols():
+    hostnames_df = DataFrame(
+        [("hostname", ["forums.news.cnn.com.ac", "forums.news.cnn.ac", "b.cnn.com"])]
+    )
+    input_df = DataFrame(
+        [
+            (4, ["ac", "", ""]),
+            (3, ["com", "ac", ""]),
+            (2, ["cnn", "cnn", "com"]),
+            (1, ["news", "news", "cnn"]),
+            (0, ["forums", "forums", "b"]),
+        ]
+    )
+    expected_output_df = DataFrame(
+        [
+            (4, ["ac", "", ""]),
+            (3, ["com", "ac", ""]),
+            (2, ["cnn", "cnn", "com"]),
+            (1, ["news", "news", "cnn"]),
+            (0, ["forums", "forums", "b"]),
+            ("tld4", ["ac", "", ""]),
+            ("tld3", ["com.ac", "ac", ""]),
+            ("tld2", ["cnn.com.ac", "cnn.ac", "com"]),
+            ("tld1", ["news.cnn.com.ac", "news.cnn.ac", "cnn.com"]),
+            ("tld0", ["forums.news.cnn.com.ac", "forums.news.cnn.ac", "b.cnn.com"]),
+        ]
+    )
+    col_len = len(input_df.columns) - 1
+    actual_output_df = dns.generate_tld_cols(
+        input_df, hostnames_df["hostname"], col_len
+    )
+    assert expected_output_df.equals(actual_output_df)
+
+
+@pytest.mark.parametrize("input_df", [input_df])
+def test_parse_url_invalid_req_cols(input_df):
+    expected_error = ValueError(
+        "Given req_cols must be subset of %s"
+        % (["hostname", "subdomain", "domain", "suffix"])
+    )
+    with pytest.raises(ValueError) as actual_error:
+        output_df = dns.parse_url(input_df["url"], req_cols=["test"])
+        assert actual_error == expected_error
