84932ef6b36f55e9b32285d2d13a6d583593c085
bsuryadevara <bhargavsuryadevara@gmail.com>
2019-09-11 18:23:51 -0500

    Updated test class with more input data samples.

--- rapidscyber/tests/test_splunk_notable_parser.py
+++ rapidscyber/tests/test_splunk_notable_parser.py
@@ -5,7 +5,11 @@ from rapidscyber.parsers.splunk_notable_parser import SplunkNotableParser
 TEST_DATA = '1566345812.924, search_name="Test Search Name", orig_time="1566345812.924", info_max_time="1566346500.000000000", info_min_time="1566345300.000000000", info_search_time="1566305689.361160000", message.description="Test Message Description", message.hostname="msg.test.hostname", message.ip="100.100.100.123", message.user_name="user@test.com", severity="info", urgency="medium"'
 TEST_DATA2 = '1548772230, search_name="Test Search Name 2", signature="Android.Adware.Batmobil", signature="Android.Adware.Dlv", signature="Android.PUP.Downloader", src="10.01.01.123", src="10.01.01.1", src_ip="10.01.01.123", src_ip="10.01.01.1, count="19", info_max_time="1548772200.000000000", info_min_time="1548599400.000000000", info_search_time="1548772206.179561000", info_sid="test-info-sid", lastTime="1548771235", orig_raw="<164>fenotify-113908.warning: CEF:0|FireEye|MPS|1.2.3.123|RC|riskware-callback|1|rt=Jan 29 2019 14:13:55 UTC end=Jan 29 2019 14:13:55 UTC src=10.01.01.123 dest="10.01.01.122" request=http://test.com/test.php cs1Label=sname cs1=Android.PUP.Downloader act=notified dvc=10.01.01.2 dvchost=fireeye.ban2-in smac=1a:2b:3c:4d:5e:6f dmac=1a:2b:3c:4d:5e:7g spt=49458 dpt=80 cn1Label=vlan cn1=0 externalId=123456 devicePayloadId=123abc msg=risk ware detected:57007 proto=tcp cs4Label=link cs4=https://fireeye.test/notification_url/test cs6Label=channel cs6=POST /multiadctrol.php HTTP/1.1::~~Content-type: application/json::~~User-Agent: Dalvik/2.1.0 (Linux; U; Android 8.0.0; SM-G611F Build/R16NW)::~~Host: test.hostname::~~Connection: Keep-Alive::~~Accept-Encoding: gzip::~~Content-Length: 85::~~::~~[{"android_id":"123abc","isnew":0,"m_ch":"123","s_ch":"1","ver_c":"342\\"}] \n\\\\x00", orig_sourcetype="source", src_subnet="12.34.56"'
 TEST_DATA3 = '1548234811, search_name="Test Search Name 3", signature="Android.Adware.Batmobil", signature="Android.Adware.Dlv", signature="Android.PUP.Downloader", src="10.01.01.123", src="10.01.01.1", count="19", info_max_time="1548234811.000000000", info_min_time="1548599400.000000000", info_search_time="1548772206.179561000", info_sid="test-info-sid", lastTime="1548771235", orig_raw="<164>fenotify-113908.warning: CEF:0|FireEye|MPS|1.2.3.123|RC|riskware-callback|1|rt=Jan 29 2019 14:13:55 UTC end=Jan 29 2019 14:13:55 UTC src=10.01.01.123 dest="10.01.01.122" request=http://test.com/test.php cs1Label=sname cs1=Android.PUP.Downloader act=notified dvc=10.01.01.2 dvchost=fireeye.ban2-in smac=1a:2b:3c:4d:5e:6f dmac=1a:2b:3c:4d:5e:7g spt=49458 dpt=80 cn1Label=vlan cn1=0 externalId=123456 devicePayloadId=123abc msg=risk ware detected:57007 proto=tcp cs4Label=link cs4=https://fireeye.test/notification_url/test cs6Label=channel cs6=POST /multiadctrol.php HTTP/1.1::~~Content-type: application/json::~~User-Agent: Dalvik/2.1.0 (Linux; U; Android 8.0.0; SM-G611F Build/R16NW)::~~Host: test.hostname::~~Connection: Keep-Alive::~~Accept-Encoding: gzip::~~Content-Length: 85::~~::~~[{"android_id":"123abc","isnew":0,"m_ch":"123","s_ch":"1","ver_c":"342\\"}] \n\\\\x00", orig_sourcetype="source", src_subnet="12.34.56"'
-
+TEST_DATA4 = '1566345700, search_name="Endpoint - Brute Force against Known User - Rule", orig_source="100.20.2.21", orig_source="FEDEX-MA", orig_source="localhost.com", failure="1104", first="Pattrick", identity="pjame", info_max_time="1546382400.000000000", info_min_time="1546378800.000000000", info_search_time="1546382850.589570000", success="8", user="pjame'
+TEST_DATA5 = '1566345700, search_name="Manual Notable Event - Rule", _time="1554290847", app="SplunkEnterpriseSecuritySuite", creator="test@nvidia.com", info_max_time="+Infinity", info_min_time="0.000", info_search_time="1554290847.423961000", owner="test@nvidia.com", rule_description="FireEye NX alert for Incident Review with Major Severity", rule_title="FireEye NX alert for Incident Review(Majr)", security_domain="endpoint", status="0", urgency="medium"'
+TEST_DATA6 = '1566345700, search_name="Endpoint - FireEye NX alert for Incident Review (Minor) - Rule", category="riskware-callback", dest_ip="10.15.90.150", occurred="Mar 09 2019 02:36:00 UTC", signature="Android.Adware.Batmobil", src_ip="10.15.90.151", src_port="40472", orig_time="1552098960", info_max_time="1552099380.000000000", info_min_time="1552098780.000000000", info_search_time="1552052094.393543000", severity="minr", src_host="ip-10.5.13.compute.internal"'
+TEST_DATA7 = '1566345700, search_name="Endpoint - FireEye NX alert for Incident Review (Minor) - Rule", category="riskware-callback", dest_ip="10.15.90.150", occurred="Mar 09 2019 02:36:00 UTC", signature="Android.Adware.Batmobil", src_ip="10.15.90.151", dest_port="80", orig_time="1552098960", info_max_time="1552099380.000000000", info_min_time="1552098780.000000000", info_search_time="1552052094.393543000", severity="minr", src_host="ip-10.5.13.compute.internal"'
+TEST_DATA8 = '1566345700, search_name=\\"Endpoint - Host With Malware Detected (Quarantined or Waived) - Rule\\", count=\\"1\\", dest=\\"TEST-01\\", dest_priority=\\"medium\\", info_max_time=\\"1511389440.000000000\\", info_min_time=\\"1511388840.000000000\\", info_search_time=\\"1511389197.841039000\\", info_sid=\\"rt_scheduler_dGNhcnJvbGxAbnZpZGlhLmNvbQ__SplunkEnterpriseSecuritySuite__RMD5c5145919d43bdffc_at_1511389196_22323\\", lastTime=\\"1511388996.202094\\"'
 
 def test_splunk_notable_parser():
     """Test splunk notable parsing"""
@@ -102,3 +106,35 @@ def test_splunk_notable_parser():
     assert test_output_df3["message_username"][0] == ""
     assert test_output_df3["message_hostname"][0] == ""
     assert test_output_df3["message_description"][0] == ""
+
+    test_input_df4 = cudf.DataFrame()
+    test_input_df4[raw_colname] = [TEST_DATA4]
+    test_output_df4 = snp.parse(test_input_df4, raw_colname)
+    assert len(test_output_df4.columns) == 24
+    assert test_output_df4["user"][0] == "pjame"
+    
+    test_input_df5 = cudf.DataFrame()
+    test_input_df5[raw_colname] = [TEST_DATA5]
+    test_output_df5 = snp.parse(test_input_df5, raw_colname)
+    assert len(test_output_df5.columns) == 24
+    assert test_output_df5["owner"][0] == "test@nvidia.com"
+    assert test_output_df5["security_domain"][0] == "endpoint"
+    
+    test_input_df6 = cudf.DataFrame()
+    test_input_df6[raw_colname] = [TEST_DATA6]
+    test_output_df6 = snp.parse(test_input_df6, raw_colname)
+    assert len(test_output_df6.columns) == 24
+    assert test_output_df6["src_port"][0] == "40472"
+    
+    test_input_df7 = cudf.DataFrame()
+    test_input_df7[raw_colname] = [TEST_DATA7]
+    test_output_df7 = snp.parse(test_input_df7, raw_colname)
+    assert len(test_output_df7.columns) == 24
+    assert test_output_df7["dest_port"][0] == "80"
+    
+    test_input_df8 = cudf.DataFrame()
+    test_input_df8[raw_colname] = [TEST_DATA8]
+    test_output_df8 = snp.parse(test_input_df8, raw_colname)
+    assert len(test_output_df8.columns) == 24
+    assert test_output_df8["dest_priority"][0] == "medium"
+    
