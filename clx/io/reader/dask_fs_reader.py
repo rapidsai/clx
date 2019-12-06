8f055bb0608318fbbc120319139e9a7119c7dd2f
Bianca Rhodes <brhodes@nvidia.com>
2019-12-06 20:19:57 +0000

    Moved change to unit test

--- clx/io/reader/dask_fs_reader.py
+++ clx/io/reader/dask_fs_reader.py
@@ -41,7 +41,7 @@ class DaskFileSystemReader(FileReader):
         del kwargs["input_path"]
 
         if "parquet" == input_format:
-            df = dask_cudf.read_parquet(filepath, gather_statistics=False, split_row_groups=False, **kwargs)
+            df = dask_cudf.read_parquet(filepath, **kwargs)
         elif "orc" == input_format:
             df = dask_cudf.read_orc(filepath, engine="cudf")
         else:
