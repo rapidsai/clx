7c19810cfbb4ce46d4b52408317937ae1e5dfb4f
Bianca Rhodes <brhodes@nvidia.com>
2020-03-02 09:59:35 -0800

    Rename file

--- examples/streamz/scripts/entry.sh
+++ examples/streamz/scripts/entry.sh
@@ -24,7 +24,7 @@ $KAFKA_HOME/bin/kafka-console-producer.sh --broker-list $BROKER --topic $TOPIC <
 source activate rapids
 
 # Check the environment.
-python /python/env_test.py
+python /python/check_env.py
 
 # Run cybert
 python -i /python/cybert.py
