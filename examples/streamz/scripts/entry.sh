a61a84b0775628c7ab4065bd7ed9337a4e7aec8b
Bianca Rhodes <brhodes@nvidia.com>
2020-03-06 19:59:25 -0800

    Update entry script

--- examples/streamz/scripts/entry.sh
+++ examples/streamz/scripts/entry.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set +e
 
+source activate rapids
+
 BROKER="localhost:9092"
 GROUP_ID="streamz"
 ENV_TEST_SCRIPT="/python/check_env.py"
 INPUT_TOPIC="input"
-INPUT_TOPIC="output"
+OUTPUT_TOPIC="output"
 
 #If sample data filepath is not passed as a parameter, use sample.csv data.
 if [ -n "$1" ]; then
@@ -14,10 +16,9 @@ else
   SAMPLE_DATA="/data/sample.csv"
 fi
 
-echo $JAVA_HOME
-
 # Start Zookeeper
 $KAFKA_HOME/bin/zookeeper-server-start.sh -daemon $KAFKA_HOME/config/zookeeper.properties
+sleep 3
 
 # Configure Kafka
 sed -i '/#listeners=PLAINTEXT:\/\/:9092/c\listeners=PLAINTEXT:\/\/localhost:9092' $KAFKA_HOME/config/server.properties
@@ -25,6 +26,7 @@ sed -i '/#advertised.listeners=PLAINTEXT:\/\/your.host.name:9092/c\advertised.li
 
 # Run Kafka
 $KAFKA_HOME/bin/kafka-server-start.sh -daemon $KAFKA_HOME/config/server.properties
+sleep 3
 
 # Create kafka topic
 $KAFKA_HOME/bin/kafka-topics.sh --create --bootstrap-server $BROKER --replication-factor 1 --partitions 1 --topic $INPUT_TOPIC
@@ -32,8 +34,6 @@ $KAFKA_HOME/bin/kafka-topics.sh --create --bootstrap-server $BROKER --replicatio
 
 # Read sample data into the kafka topic
 $KAFKA_HOME/bin/kafka-console-producer.sh --broker-list $BROKER --topic $INPUT_TOPIC < $SAMPLE_DATA
-
-source activate rapids
 
 # Check the environment.
 python $ENV_TEST_SCRIPT
