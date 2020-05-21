a933096af0b802587f4cf8f7d698a9e7c8adb75b
Eli Fajardo <efajardo@nvidia.com>
2020-05-21 00:02:28 +0000

    update/clean build scripts

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -43,15 +43,13 @@ python --version
 conda config --set ssl_verify False
 
 logger "conda install required packages"
-conda install -y -c pytorch -c rapidsai/label/pytorch pytorch=1.3.1 torchvision=0.4.2
-conda install -y -c nvidia -c rapidsai -c rapidsai-nightly -c conda-forge -c defaults \
+conda install -y -c conda-forge -c rapidsai -c rapidsai-nightly -c nvidia \
+    "cupy>=7,<8.0.0a0" \
     "cugraph=${MINOR_VERSION}" \
     "cuxfilter=${MINOR_VERSION}" \
-    "cupy>=6.6.0,<8.0.0a0,!=7.1.0" \
     "dask>=2.8.0" \
     "distributed>=2.8.0" \
     "dask-cudf=${MINOR_VERSION}" \
-    "seaborn" \
     "s3fs" \
     "nodejs"
 
