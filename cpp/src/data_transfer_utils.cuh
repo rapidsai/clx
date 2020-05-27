490a5406e1a8eac86f897f2293d700161e5a63cf
Bianca Rhodes <brhodes@nvidia.com>
2020-05-27 19:37:12 +0000

    remove malloc_and_copy_vec_to_device

--- cpp/src/data_transfer_utils.cuh
+++ cpp/src/data_transfer_utils.cuh
@@ -9,17 +9,11 @@
 
 static void gpuCheck(cudaError_t err, const char *file, int line) {
   if (err != cudaSuccess) {
-      std::cerr << cudaGetErrorString(err) << " in file " << file << " at line " 
+      std::cerr << cudaGetErrorString(err) << " in file " << file << " at line "
                                            << line << "." << std::endl;
       exit(1);
   }
 }
 
 #define assertCudaSuccess(cu_err) {gpuCheck((cu_err), __FILE__, __LINE__);}
-
-template<typename T>
-void malloc_and_copy_vec_to_device(rmm::device_vector<T>& dest, std::vector<T> const& vec) {
-  dest.resize(vec.size());
-  thrust::copy(vec.begin(), vec.end(), dest.begin());
-}
 
