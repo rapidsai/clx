fcfe7123aaf2669f687bf5e602cbf75c15db9375
Bianca Rhodes <brhodes@nvidia.com>
2020-06-04 15:45:25 +0000

    Added google benchmark and gtest

--- cpp/benchmarks/tokenizer_benchmark.cu
+++ cpp/benchmarks/tokenizer_benchmark.cu
@@ -6,7 +6,7 @@
 #define MAX_ROWS_TENSOR 300
 
 static void BM_cuda_tokenizer_file(benchmark::State& state) {
-  std::string input_file_name = "cpp/benchmark/tokenizer_test.txt";
+  std::string input_file_name = "cpp/benchmarks/tokenizer_benchmark.txt";
   std::string hash_file = "python/clx/analytics/resources/bert_hash_table.txt";
   uint32_t max_sequence_length = 64;
   uint32_t stride = 48;
