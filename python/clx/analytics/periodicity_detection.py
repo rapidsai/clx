8b607458b3680ff7fdd15b8d4980194587f64df8
Eli Fajardo <efajardo@nvidia.com>
2020-11-09 20:29:39 +0000

    periodicity detection api doc

--- python/clx/analytics/periodicity_detection.py
+++ python/clx/analytics/periodicity_detection.py
@@ -2,6 +2,14 @@ import cupy as cp
 
 
 def to_periodogram(signal):
+    """
+    Returns periodogram of signal for finding frequencies that have high energy.
+
+    :param signal: signal (time domain)
+    :type signal: cudf.Series
+    :return: CuPy array representing periodogram
+    :rtype: cupy.core.core.ndarray
+    """
 
     # convert cudf series to cupy array
     signal_cp = cp.fromDlpack(signal.to_dlpack())
@@ -13,12 +21,20 @@ def to_periodogram(signal):
     FFT_data = cp.fft.fft(signal_cp_std)
 
     # create periodogram
-    prdg = (1 / len(signal)) * ((cp.absolute(FFT_data))**2)
+    prdg = (1 / len(signal)) * ((cp.absolute(FFT_data)) ** 2)
 
     return prdg
 
 
 def filter_periodogram(prdg, p_value):
+    """
+    Select important frequencies by filtering periodogram by p-value. Filtered out frequencies are set to zero.
+
+    :param periodogram: periodogram to be filtered
+    :type signal: cudf.Series
+    :return: CuPy array representing periodogram
+    :rtype: cupy.core.core.ndarray
+    """
 
     filtered_prdg = cp.copy(prdg)
     filtered_prdg[filtered_prdg < (cp.mean(filtered_prdg) * (-1) * (cp.log(0.001)))] = 0
@@ -27,6 +43,14 @@ def filter_periodogram(prdg, p_value):
 
 
 def to_time_domain(prdg):
+    """
+    Convert periodogram to signal in time domain.
+
+    :param periodogram: periodogram (frequency domain)
+    :type signal: cupy.core.core.ndarray
+    :return: CuPy array representing reconstructed signal
+    :rtype: cupy.core.core.ndarray
+    """
 
     acf = cp.abs(cp.fft.ifft(prdg))
 
