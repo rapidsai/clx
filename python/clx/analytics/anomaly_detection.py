75fb599aba28fa7349d2de75244c11a0c5574537
Bianca Rhodes <brhodes@nvidia.com>
2021-03-04 17:53:45 +0000

    update anomaly detection

--- python/clx/analytics/anomaly_detection.py
+++ python/clx/analytics/anomaly_detection.py
@@ -2,33 +2,40 @@ import cudf
 import cuml
 
 
-def cluster(feature_dataframe, min_samples=3, eps=0.3):
+def dbscan(feature_dataframe, min_samples=3, eps=0.3):
     """
-        Pass a feature dataframe to this function to detect anomalies in your datafeature dataframe and outputs associated labels 0,1,-1.
-        Parameters
-        ----------
-        :param feature_dataframe: Feature dataframe to be used for clustering
-        :type feature_dataframe: cudf.DataFrame
+    Pass a feature dataframe to this function to detect anomalies in your feature dataframe. This function uses ``cugraph`` DBSCAN to detect anomalies
+    and outputs associated labels 0,1,-1.
 
-        Examples
-        --------
-        >>> import cudf
-        >>> from clx.analytics.cluster import anomaly_detection
-        >>> df = cudf.DataFrame(
-        >>>     {
-        >>>         "time": [1, 2, 3, 4, 5, 6, 7],
-        >>>         "user": ["u1", "u2", "u3", "u1", "u1", "u2", "u1"],
-        >>>         "computer": ["c1", "c2", "c3", "c1", "c2", "c3", "c1"],
-        >>>     }
-        >>> )
-        >>> feature_df = clx.features.frequency(df, entity_id="user", feature_id="computer")
-        >>> labels = anomaly_detection(feature_df)
-        >>> labels
-            0   -1
-            1   -1
-            2   -1
-            dtype: int32
-        """
+    Parameters
+    ----------
+    :param feature_dataframe: Feature dataframe to be used for clustering
+    :type feature_dataframe: cudf.DataFrame
+    :param min_samples: Minimum samples to use for dbscan
+    :type min_samples: int
+    :param eps: Max distance to use for dbscan
+    :type eps: float
+
+    Examples
+    --------
+    >>> import cudf
+    >>> import clx.features
+    >>> import clx.analytics.anomaly_detection
+    >>> df = cudf.DataFrame(
+    >>>         {
+    >>>             "time": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
+    >>>             "user": ["u1","u1","u1","u1","u1","u1","u1","u1","u1","u1","u5","u4","u2","u3"],
+    >>>             "computer": ["c1","c2","c3","c1","c2","c3","c1","c1","c2","c3","c1","c1","c5","c6"],
+    >>>         }
+    >>>     )
+    >>> feature_df = clx.features.frequency(df, entity_id="user", feature_id="computer")
+    >>> labels = clx.analytics.anomaly_detection.dbscan(feature_df, min_samples=2, eps=0.5)
+    >>> labels
+        0   -1
+        1   -1
+        2   -1
+        dtype: int32
+    """
     dbscan = cuml.cluster.DBSCAN(eps=eps, min_samples=min_samples)
     dbscan.fit(feature_dataframe)
     # return anomalies only
--- python/clx/tests/test_anomaly_detection.py
+++ python/clx/tests/test_anomaly_detection.py
@@ -10,43 +10,6 @@ def test_anomaly_detection():
             "time": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
             "user": [
                 "u1",
-                "u1",
-                "u1",
-                "u1",
-                "u1",
-                "u1",
-                "u1",
-                "u1",
-                "u1",
-                "u1",
-                "u5",
-                "u4",
-                "u2",
-                "u3",
-            ],
-            "computer": [
-                "c1",
-                "c2",
-                "c3",
-                "c1",
-                "c2",
-                "c3",
-                "c1",
-                "c1",
-                "c2",
-                "c3",
-                "c1",
-                "c1",
-                "c5",
-                "c6",
-            ],
-        }
-    )
-    df = cudf.DataFrame(
-        {
-            "time": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
-            "user": [
-                "u1",
                 "u5",
                 "u4",
                 "u2",
@@ -80,7 +43,7 @@ def test_anomaly_detection():
         }
     )
     fdf = clx.features.frequency(df, "user", "computer")  # Create feature data
-    actual = clx.analytics.anomaly_detection.cluster(fdf, min_samples=2, eps=0.5)
+    actual = clx.analytics.anomaly_detection.dbscan(fdf, min_samples=2, eps=0.5)
     expected = cudf.Series([-1, -1], dtype="int32")
     expected.index = cudf.Series([0, 3])
     assert actual.equals(expected)
