eb7642d7f6b216a65f9dd39e205bd36469d0c2bc
Tad ZeMicheal <thzemicheal@gmail.com>
2021-03-25 19:40:27 +0000

    pr issues fixed

--- notebooks/ids_detection/util.py
+++ notebooks/ids_detection/util.py
@@ -35,3 +35,17 @@ def plot_pr(label, y_scores):
     plt.xlabel('Recall')
     plt.legend(loc='best')
     plt.title('Area under PR curve')
+
+def missing_values_table(df):
+    mis_val = df.isnull().sum()
+    mis_val_percent = 100 * df.isnull().sum() / len(df)
+    mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
+    mis_val_table_ren_columns = mis_val_table.rename(
+    columns = {0 : 'Missing Values', 1 : '% of Total Values'})
+    mis_val_table_ren_columns = mis_val_table_ren_columns[
+        mis_val_table_ren_columns.iloc[:,1] != 0].sort_values(
+    '% of Total Values', ascending=False).round(1)
+    print ("Your selected dataframe has " + str(df.shape[1]) + " columns.\n"      
+        "There are " + str(mis_val_table_ren_columns.shape[0]) +
+            " columns that have missing values.")
+    return mis_val_table_ren_columns
