0e6ef81841ff4c84d7d576d6a3e9daad1a78e907
shaneding <shane200195@gmail.com>
2021-06-15 16:01:17 -0400

    Added custreamz and cugraph integration demo (#405)
    Authors:
      - https://github.com/shaneding
    
    Approvers:
      - https://github.com/raykallen
      - AJ Schmidt (https://github.com/ajschmidt8)
    
    URL: https://github.com/rapidsai/clx/pull/405

--- ci/gpu/test-notebooks.sh
+++ ci/gpu/test-notebooks.sh
@@ -10,7 +10,7 @@ TOPLEVEL_NB_FOLDERS=$(find . -name *.ipynb |cut -d'/' -f2|sort -u)
 
 # Add notebooks that should be skipped here
 # (space-separated list of filenames without paths)
-SKIPNBS="FLAIR_DNS_Log_Parsing.ipynb CLX_Workflow_Notebook2.ipynb CLX_Workflow_Notebook3.ipynb Supervised_Asset_Classification.ipynb CLX_Supervised_Asset_Classification.ipynb DGA_Detection.ipynb Predictive_Maintenance_Sequence_Classifier.ipynb IDS_using_LODA.ipynb anomalous_behavior_profiling_supervised.ipynb"
+SKIPNBS="FLAIR_DNS_Log_Parsing.ipynb CLX_Workflow_Notebook2.ipynb CLX_Workflow_Notebook3.ipynb Supervised_Asset_Classification.ipynb CLX_Supervised_Asset_Classification.ipynb DGA_Detection.ipynb Predictive_Maintenance_Sequence_Classifier.ipynb IDS_using_LODA.ipynb anomalous_behavior_profiling_supervised.ipynb custream_n_graph.ipynb"
 
 ## Check env
 env
